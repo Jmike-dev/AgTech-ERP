5cc3e60e0ae2ae4e5951e8804908feb2c6ce56ee
Jmike-dev <gmikekamau@gmail.com>
2025-09-03 11:27:27 +0300

    removed test and installed swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('AgTech-ERP API')
+    .setDescription(
+      'this API is meant handle all request in a medical recorder request for a nurse in an institution ',
+    )
+    .setVersion('1.0')
+    .addTag('Medical recorder')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
