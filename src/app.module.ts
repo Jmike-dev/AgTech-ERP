0aee5c768c9f596d0cab48c22d4e470402f4bbe0
Jmike-dev <gmikekamau@gmail.com>
2025-09-03 11:18:01 +0300

    created modules that will be used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { FarmerModule } from './farmer/farmer.module';
+import { CropsModule } from './crops/crops.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, FarmerModule, CropsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
