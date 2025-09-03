5487befeb26a4c641b4fb607eb2fc28a183dd57f
Jmike-dev <gmikekamau@gmail.com>
2025-09-03 15:27:00 +0300

    farmer creation now working

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -38,20 +38,4 @@ export class AdminService {
     const { password, refreshToken, ...nurseWithoutPassword } = nurse;
     return nurseWithoutPassword;
   }
-
-  findAll() {
-    return `This action returns all admin`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} admin`;
-  }
-
-  update(id: number, updateAdminDto: UpdateAdminDto) {
-    return `This action updates a #${id} admin`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} admin`;
-  }
 }
