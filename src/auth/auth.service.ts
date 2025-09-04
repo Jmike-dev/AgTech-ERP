a4f98e2caa6a3786020f3bf2d0300111213c6577
Jmike-dev <gmikekamau@gmail.com>
2025-09-04 11:33:52 +0300

    created simple login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  async validateAdmin(email: string, password: string) {
+    const admin = await this.prisma.admin.findUnique({
+      where: { email },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+    if (!admin) {
+      throw new BadRequestException('Invalid email or password');
+    }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+    const isPasswordValid = await bcrypt.compare(password, admin.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    // return admin details without password
+    const { password: _, ...adminWithoutPassword } = admin;
+    return adminWithoutPassword;
+  }
+  async validateFarmer(email: string, password: string) {
+    const farmers = await this.prisma.farmers.findUnique({
+      where: { email },
+    });
+
+    if (!farmers) {
+      throw new BadRequestException('Invalid email or password');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, farmers.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    // return farmers details without password
+    const { password: _, ...adminWithoutPassword } = farmers;
+    return adminWithoutPassword;
   }
 }
